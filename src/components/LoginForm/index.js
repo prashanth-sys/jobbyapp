719afddd87406ab7772692de188e9102008f185c
Your Name <reddyprashanth512@gmail.com>
2023-12-28 08:22:41 +0000

    solution

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <h1>hii</h1>
+      </div>
+    )
+  }
+}
+export default LoginForm
